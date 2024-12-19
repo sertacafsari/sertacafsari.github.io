784c1f9b0b6b22ee43a6c5eafcb8ed1975829c2d
sertacafsari <s.b.afsari@student.rug.nl>
2024-12-19 17:59:06 +0100

    Few configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <BrowserRouter basename="/sertacafsari.github.io">
-      <App />
-    </BrowserRouter>);
+  <BrowserRouter basename="/sertacafsari.github.io">
+    <App />
+  </BrowserRouter>
+);
